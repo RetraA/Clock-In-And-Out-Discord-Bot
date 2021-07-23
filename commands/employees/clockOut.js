420cd166cbcd78e6ad8cfff908d2607904bf93d0
RetraA <50384498+RetraA@users.noreply.github.com>
2021-07-23 13:59:57 -0400

    made embed

--- commands/employees/clockOut.js
+++ commands/employees/clockOut.js
@@ -1,4 +1,5 @@
 const map = require('../../index.js').map
+const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "clockout",
@@ -13,15 +14,27 @@ module.exports = {
            message.reply(` You haven't clocked in yet!`)
            return
        }
-       map.delete(message.author.id)
-
        const diff = clockOut.getTime() - clockIn.getTime()
        const seconds = diff/1000;
        const minutes= seconds / 60;
        const finalTime = Math.round(minutes);
+
+       map.delete(message.author.id)
+       let sEmbed = new MessageEmbed()
+       .setColor(`YELLOW`)
+       .setTitle("Clock Out Log")
+       .setTimestamp()
+       .setThumbnail('https://i.imgur.com/3xgt3jg.png')
+       .addField("**User:**", `${message.author}`)
+       .addField(`**Total Minutes Worked:**`, `${finalTime}`)
+       .setFooter(`by Retra#1337`, 'https://i.imgur.com/3xgt3jg.png')
+      let timeClock = client.channels.cache.get(`679832576571080740`);
+        timeClock.send(sEmbed);
+        message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
+
        
-       let timeClock = client.channels.cache.get(`<your log channel id>`);
-       message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
-       timeClock.send(`${message.author} clocked out and worked ${finalTime} minutes`);
+      // let timeClock = client.channels.cache.get(`679670897728290846`);
+       //message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
+       //timeClock.send(`${message.author} clocked out and worked ${finalTime} minutes`);
     }
 }
