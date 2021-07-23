1b5d37132d4abd11cca3181b24008c749c0ae3ae
RetraA <50384498+RetraA@users.noreply.github.com>
2021-07-23 14:01:29 -0400

    Update clockOut.js

--- commands/employees/clockOut.js
+++ commands/employees/clockOut.js
@@ -28,7 +28,7 @@ module.exports = {
        .addField("**User:**", `${message.author}`)
        .addField(`**Total Minutes Worked:**`, `${finalTime}`)
        .setFooter(`by Retra#1337`, 'https://i.imgur.com/3xgt3jg.png')
-      let timeClock = client.channels.cache.get(`679832576571080740`);
+      let timeClock = client.channels.cache.get(`866501334123413535`);
         timeClock.send(sEmbed);
         message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
 
