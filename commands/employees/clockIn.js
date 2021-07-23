738e657905a2880e71792992b0a4aa9bb6345ce6
RetraA <50384498+RetraA@users.noreply.github.com>
2021-07-23 14:08:46 -0400

    Update clockIn.js

--- commands/employees/clockIn.js
+++ commands/employees/clockIn.js
@@ -21,7 +21,7 @@ module.exports = {
        .setThumbnail('https://i.imgur.com/3xgt3jg.png')
        .addField("**User:**", `${message.author} has clocked in!`)
        .setFooter(`by Retra#1337`, 'https://i.imgur.com/3xgt3jg.png')
-        let timeClock = client.channels.cache.get(`866501334123413535`);
+        let timeClock = client.channels.cache.get(`your channel id`);
         timeClock.send(sEmbed);
 
 
