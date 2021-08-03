c0c60f5e9fde105bb1a351569d8db7c2a99c9520
Retra <50384498+RetraA@users.noreply.github.com>
2021-08-02 20:16:41 -0400

    cleaned up

--- commands/employees/clockOut.js
+++ commands/employees/clockOut.js
@@ -9,32 +9,32 @@ module.exports = {
     run: async(client, message, args) => {
         const clockOut = new Date(Date.now());
 
-       const clockIn = map.get(message.author.id)
-       if (clockIn == null) {
-           message.reply(` You haven't clocked in yet!`)
-           return
-       }
-       const diff = clockOut.getTime() - clockIn.getTime()
-       const seconds = diff/1000;
-       const minutes= seconds / 60;
-       const finalTime = Math.round(minutes);
+        const clockIn = map.get(message.author.id)
+        if (clockIn == null) {
+            message.reply(` You haven't clocked in yet!`)
+            return
+        }
+        const diff = clockOut.getTime() - clockIn.getTime()
+        const seconds = diff / 1000;
+        const minutes = seconds / 60;
+        const finalTime = Math.round(minutes);
 
-       map.delete(message.author.id)
-       let sEmbed = new MessageEmbed()
-       .setColor(`YELLOW`)
-       .setTitle("Clock Out Log")
-       .setTimestamp()
-       .setThumbnail(`${message.author.avatarURL()}`)
-       .addField("**User:**", `${message.author}`)
-       .addField(`**Total Minutes Worked:**`, `${finalTime}`)
-       .setFooter(`by Retra#1337`, 'https://i.imgur.com/3xgt3jg.png')
-      let timeClock = client.channels.cache.get(`your channel id`);
+        map.delete(message.author.id)
+        let sEmbed = new MessageEmbed()
+            .setColor(`YELLOW`)
+            .setTitle("Clock Out Log")
+            .setTimestamp()
+            .setThumbnail(`${message.author.avatarURL()}`)
+            .addField("**User:**", `${message.author}`)
+            .addField(`**Total Minutes Worked:**`, `${finalTime}`)
+            .setFooter(`by Retra#1337`, 'https://i.imgur.com/3xgt3jg.png')
+
+        let timeClock = client.channels.cache.get(`your channel id`);
         timeClock.send(sEmbed);
-        message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
+        message.author.send(`${message.author} you have clocked out!`).then(message.delete({
+            timeout: 5000
+        }));
 
-       
-      // let timeClock = client.channels.cache.get(`679670897728290846`);
-       //message.author.send(`${message.author} you have clocked out!`).then(message.delete({timeout: 5000}));
-       //timeClock.send(`${message.author} clocked out and worked ${finalTime} minutes`);
+
     }
 }
